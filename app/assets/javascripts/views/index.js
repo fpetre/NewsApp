8abab4eff20ba343a52c7b48d062da221b13c48d
App Academy Student <student@appacademy.io>
2014-10-21 15:22:31 -0400

    Add delete buttons. Add refresh buttons. Begin form for new feed.

--- app/assets/javascripts/views/index.js
+++ app/assets/javascripts/views/index.js
@@ -1,13 +1,33 @@
 NewsReader.Views.Index = Backbone.View.extend({
   template: JST["index"],
 
+  events: {
+    "click a.delete": "confirmDelete"
+  },
+
   initialize: function(){
     this.listenTo(this.collection, "sync", this.render);
+    this.listenTo(this.collection, "remove", this.render);
   },
 
   render: function() {
     var template = this.template({collection: this.collection});
     this.$el.html(template);
     return this;
+  },
+
+  confirmDelete: function(event) {
+    event.preventDefault();
+    var model = this.collection.getOrFetch($(event.currentTarget).attr("id"));
+
+    if (window.confirm("This will delete the " + model.escape("title") + "feed.")) {
+      this.deleteFeed(model);
+    }
+  },
+
+  deleteFeed: function(model) {
+    model.destroy();
   }
+
+
 });
--- app/assets/javascripts/views/show.js
+++ app/assets/javascripts/views/show.js
@@ -1,14 +1,22 @@
 NewsReader.Views.Show = Backbone.View.extend({
   template: JST["show"],
 
+  events: {
+    "click a.refresh" : "refresh"
+  },
+
   initialize: function() {
     this.listenTo(this.model, "sync", this.render);
   },
 
-  render: function(){
+  render: function() {
     var template = this.template({feed: this.model});
     this.$el.html(template);
     return this;
+  },
+
+  refresh: function() {
+    this.model.fetch();
   }
 
 });
